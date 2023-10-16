9a06d4155f378b6bbc9c19275d4e6bfd76230c88
Hao Zhou <hzhou.me@saltyee.com>
2023-10-16 00:44:14 -0700

    fix jackson koltin data class serialization

--- src/main/kotlin/com/saltynote/service/ServiceApplication.kt
+++ src/main/kotlin/com/saltynote/service/ServiceApplication.kt
@@ -2,6 +2,8 @@ package com.saltynote.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
@@ -13,7 +15,12 @@ import org.springframework.context.annotation.Bean
 class ServiceApplication {
     @Bean
     fun objectMapper(): ObjectMapper {
-        return ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+        return ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE).registerModule(
+            KotlinModule.Builder()
+                .configure(KotlinFeature.NullToEmptyCollection, true)
+                .configure(KotlinFeature.NullToEmptyMap, true)
+                .build()
+        )
     }
 
     companion object {

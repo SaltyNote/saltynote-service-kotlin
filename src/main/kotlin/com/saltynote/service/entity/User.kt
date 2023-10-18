ee1c514a1a50642296cc5aecd41c2f1db8ac33d6
Hao Zhou <hzhou.me@saltyee.com>
2023-10-18 00:54:02 -0700

    make username and email unique

--- src/main/kotlin/com/saltynote/service/entity/User.kt
+++ src/main/kotlin/com/saltynote/service/entity/User.kt
@@ -3,13 +3,16 @@ package com.saltynote.service.entity
 import com.saltynote.service.domain.IdentifiableUser
 import com.saltynote.service.generator.SnowflakeIdGenerator
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
 data class User(
     @Id
     private val id: Long = SnowflakeIdGenerator.DEFAULT.nextId(),
+    @Indexed(unique = true)
     private val username: String = "",
+    @Indexed(unique = true)
     val email: String = "",
     var password: String = "",
     val createdAt: Long = System.currentTimeMillis(),

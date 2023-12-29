6bf185c61f1dc582175cf7ae7409bce65e8235bb
SaltyEgg <hzhou.me@saltyee.com>
2023-12-29 14:23:40 -0800

    remove sentry

--- src/main/kotlin/com/saltynote/service/controller/advice/ExceptionHandlerControllerAdvice.kt
+++ src/main/kotlin/com/saltynote/service/controller/advice/ExceptionHandlerControllerAdvice.kt
@@ -3,7 +3,6 @@ package com.saltynote.service.controller.advice
 import cn.dev33.satoken.exception.NotLoginException
 import com.saltynote.service.domain.transfer.ServiceResponse
 import com.saltynote.service.exception.WebAppRuntimeException
-import io.sentry.Sentry
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -14,19 +13,16 @@ class ExceptionHandlerControllerAdvice {
 
     @ExceptionHandler(WebAppRuntimeException::class)
     fun handleWebClientRuntimeException(e: WebAppRuntimeException): ResponseEntity<ServiceResponse> {
-        Sentry.captureException(e)
         return ResponseEntity.status(e.status).body(ServiceResponse(e.status, e.message))
     }
 
     @ExceptionHandler(NotLoginException::class)
     fun handleWebClientRuntimeException(e: NotLoginException): ResponseEntity<ServiceResponse> {
-        Sentry.captureException(e)
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ServiceResponse(HttpStatus.FORBIDDEN, e.localizedMessage ?: "Not Login"))
     }
 
     @ExceptionHandler(RuntimeException::class)
     fun handleRuntimeException(e: RuntimeException): ResponseEntity<ServiceResponse> {
-        Sentry.captureException(e)
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(
                 ServiceResponse(
@@ -38,7 +34,6 @@ class ExceptionHandlerControllerAdvice {
 
     @ExceptionHandler(Exception::class)
     fun handleRuntimeException(e: Exception): ResponseEntity<ServiceResponse> {
-        Sentry.captureException(e)
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(
                 ServiceResponse(

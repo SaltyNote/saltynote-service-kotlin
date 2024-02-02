427af9daf2076b955f909b8de9407a9ce32807d6
SaltyEgg <hzhou.me@saltyee.com>
2024-02-02 12:39:56 -0800

    fix detekt conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     id("org.springframework.boot") version "3.1.7"
     id("io.spring.dependency-management") version "1.1.4"
     id("org.graalvm.buildtools.native") version "0.9.28"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
     id("io.gitlab.arturbosch.detekt") version "1.23.4"
 }
 
